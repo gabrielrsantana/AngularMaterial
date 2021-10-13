c5776d1b212eef122d665487752ec5f8271a1514
Gabriel Rocha <58997038+gabrielrsantana@users.noreply.github.com>
2021-10-13 20:50:52 -0300

    componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
 
 
 
@@ -14,7 +17,10 @@ import { MatSliderModule } from '@angular/material/slider';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatSliderModule
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatRadioModule,
+    MatProgressBarModule
   
     
    
