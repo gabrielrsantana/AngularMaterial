624e5c5bae97b1bc733212c319cabfc2e3eb2676
Gabriel Rocha <58997038+gabrielrsantana@users.noreply.github.com>
2021-10-13 18:14:10 -0300

    inicio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'material';
+  title = 'Angular Material';
+
+
 }
